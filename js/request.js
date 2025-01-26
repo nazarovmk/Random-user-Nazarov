3a617f5f57bbbea15933c11cf5541a3a527c6126
muhammadnazar <muhammadnazarnazarov25@gmail.com>
2025-01-26 21:14:47 +0900

    Random

--- js/request.js
+++ js/request.js
@@ -1,5 +1,47 @@
 // api
-const API = 'https://randomuser.me/api/?results=9'
+const API = "https://randomuser.me/api/?results=9";
 
 // for leader
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById("overlay");
+
+const loaderToggle = (toggle) => {
+  if (toggle) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+};
+
+const getData = (resourse) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState < 4) {
+        loaderToggle(true);
+      } else if (request.readyState == 4 && request.status == 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data.results);
+        loaderToggle(false);
+      } else if (request.request == 4) {
+        reject("Error!!!");
+        loaderToggle(false);
+      }
+    });
+
+    request.open("GET", resourse);
+    request.send();
+  });
+};
+
+const reload = () => {
+  getData(API)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", reload);
